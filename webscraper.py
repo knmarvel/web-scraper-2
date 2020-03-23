9690a8e51f4b3bc56e47f6bcb22aa11de8008b1b
Karen Marvel <knmarvel@gmail.com>
2020-03-23 18:35:58 -0400

    Initial commit

---
+++ webscraper.py
@@ -0,0 +1,99 @@
+import argparse
+import re
+import requests
+import sys
+from html.parser import HTMLParser
+
+emails = []
+urls = []
+phones = []
+
+
+class MyHTMLParser(HTMLParser):
+
+    def handle_data(self, data):
+        email_finder(data)
+        url_finder(data)
+        phone_finder(data)
+
+
+def argparser(*args, **kwargs):
+    """parse a URL passed in as a command line argument"""
+    parser = argparse.ArgumentParser(
+        description="Perform transformation on input text.")
+    parser.add_argument('url',
+                        help='url of website to search')
+    return parser.parse_args()
+
+
+def text_retriever(webpage):
+    """retrieves the text of the webpage at the specified url"""
+    print("Reading website, this may take a minute")
+    r = requests.get(webpage)
+    print("Done reading website")
+    return r.text
+
+
+def info_finder(web_text):
+    """ look for email addresses, URLs, and phone numbers
+    included in the web text"""
+    html_parser = MyHTMLParser()
+    html_parser.feed(web_text)
+
+
+def email_finder(web_text):
+    e_re = r"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*"
+    emails_found = re.findall(e_re, web_text)
+    if emails_found:
+        if len(emails_found[0]) == 1:
+            emails.append(emails_found[0])
+
+
+def url_finder(web_text):
+    u_re = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|'
+    u_re += r'[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    urls_found = re.findall(u_re, web_text)
+    if urls_found:
+        urls.append(urls_found[0])
+
+
+def phone_finder(web_text):
+    """returns phone numbers found in web_text"""
+    p_re = r'1?\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})'
+    p_re += r'\W*([0-9]{4})(\se?x?t?(\d*))?'
+    phones_found = re.findall(p_re, web_text)
+    actual_phone = ""
+    if phones_found:
+        for phone in phones_found:
+            if len(phone) <= 5:
+                actual_phone = ("({}){}-{}".format(
+                    phone[0], phone[1], phone[2]))
+        phones.append(actual_phone)
+
+
+def main(*args):
+    webpage = argparser().url
+    web_text = text_retriever(webpage)
+    info_finder(web_text)
+    if phones:
+        print("Here are the phone numbers found in this website:")
+        for phone in phones:
+            print(phone)
+    else:
+        print("No phone numbers found.")
+    if emails:
+        print("Here are the email addresses found in this website:")
+        for email in emails:
+            print(email)
+    else:
+        print("No emails found.")
+    if urls:
+        print("Here are the urls found in this website:")
+        for url in urls:
+            print(url)
+    else:
+        print("No urls found.")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
